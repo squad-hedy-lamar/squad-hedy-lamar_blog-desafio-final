99b87774295e5726bfdfc1fe30569e773d809785
Karen Vicente <karenyasmin37@gmail.com>
2024-10-23 22:17:55 -0300

    Adicionando campo de busca.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from unidecode import unidecode
+from django.db.models import Q
 
 def home(request):
-    #lista todos os posts
-    return render(request, "blog/list.html")
+    query = request.GET.get('q')
+    if query:
+        query = unidecode(query)
+        query_parts = query.split()
+        q_objects = Q()
+        
+        for part in query_parts:
+            q_objects |= (Q(title__icontains=part) | Q(content__icontains=part))
+        
+        posts = Post.objects.filter(q_objects)
+    else: 
+        posts = Post.objects.all()
+        
+    return render(request, "blog/list.html", {'posts': posts, 'query': query})
 
 def details(request, id):
 

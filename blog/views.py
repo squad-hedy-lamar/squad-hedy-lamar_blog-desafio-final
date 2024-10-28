c301eca696468ecfa72cceb94cb4218a8968ca3d
Daniela2319 <frankvt04@gmail.com>
2024-10-28 16:34:37 -0300

    atualizando gerencimento de postagem

--- blog/views.py
+++ blog/views.py
@@ -7,41 +7,39 @@ from django_comments.models import Comment
 
 
 def home(request):
-    query = request.GET.get('q')
+    query = request.GET.get("q")
     if query:
         query = unidecode(query)
         query_parts = query.split()
         q_objects = Q()
-        
+
         for part in query_parts:
-            q_objects |= (Q(title__icontains=part) | Q(content__icontains=part))
-        
+            q_objects |= Q(title__icontains=part) | Q(content__icontains=part)
+
         posts = Post.objects.filter(q_objects)
-    else: 
+    else:
         posts = Post.objects.all().order_by("-created_at")
-        
-    return render(request, "blog/list.html", {'posts': posts, 'query': query})
+
+    return render(request, "blog/list.html", {"posts": posts, "query": query})
+
 
 def details(request, id):
     # Tenta buscar o post pelo ID, ou retorna 404 se não existir
-    post  = get_object_or_404(Post, id=id)
-    comments = Comment.objects.filter(object_pk=post.pk, content_type__model='post')  # Filtrar pelos comentários do post
-    return render(request, "blog/details.html", {'post': post, 'comments': comments})
+    post = get_object_or_404(Post, id=id)
+    comments = Comment.objects.filter(
+        object_pk=post.pk, content_type__model="post"
+    )  # Filtrar pelos comentários do post
+    return render(request, "blog/details.html", {"post": post, "comments": comments})
+
 
 def criar_post(request):
-    # post = Post(title=request.POST['title'], content=request.POST['content'])
-    # post.save()
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        image = request.FILES.get('image')  # Captura a imagem enviada
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        image = request.FILES.get("image")  # Captura a imagem enviada
 
         post = Post(title=title, content=content, image=image, author=request.user)
         post.save()
-        return redirect('blog-home')
+        return redirect("blog-home")
 
-    return render(request, 'blog/create_post.html')
-    
-
-
-    
+    return render(request, "blog/create_post.html")
